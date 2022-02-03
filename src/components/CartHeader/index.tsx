685739036e43e1e7405580d69ea1aec1af445d58
Lucas Becker <lucasgabrielbec@gmail.com>
2022-02-03 18:09:25 -0300

    wip: still working on cart

--- src/components/CartHeader/index.tsx
+++ src/components/CartHeader/index.tsx
@@ -1,16 +1,21 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import { MdShoppingBasket } from 'react-icons/md';
+import { useCartStore } from '../../stores/cartStore';
 import { Cart, Container } from './styles';
 
 const CartHeader: React.FC = () => {
+  const products = useCartStore(state => state.products.length);
+  const label = products === 1 ? 'Item' : 'Itens';
   return (
     <Container>
       <Cart href="/users/cart">
         <a>
           <div>
             <strong>Meu carrinho</strong>
-            <span> itens</span>
+            <span style={{ marginLeft: 10 }}>
+              {products} {label}
+            </span>
           </div>
           <MdShoppingBasket size={36} color="#ff9000" />
         </a>
