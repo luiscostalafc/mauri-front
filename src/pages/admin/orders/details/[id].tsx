6741853d9c2b887b4531ba31277d1fa99c70b330
Lucas Becker <lucasgabrielbec@gmail.com>
2022-02-24 23:02:55 -0300

    feat: done with cart

--- src/pages/admin/orders/details/[id].tsx
+++ src/pages/admin/orders/details/[id].tsx
@@ -7,11 +7,11 @@ import AdminMenu from '../../../../components/Admin/Menu';
 
 export default function Details() {
   const router = useRouter();
-  const { id } = router.query;
   const [data, setData] = useState([]);
 
   useEffect(() => {
     async function getData() {
+      const { id } = router.query;
       const res = await fetch(
         `${process.env.NEXT_PUBLIC_POSTGRES_URI}/order-details/search/${id}`,
       );
@@ -21,14 +21,22 @@ export default function Details() {
         setData(data);
       }
     }
-
     getData();
   }, []);
 
   const orderStatuses = {
-    pending: 'Pendente',
-    approved: 'Aprovada',
-    failure: 'Falha',
+    pending: {
+      bg: 'orange',
+      text: 'Pendente',
+    },
+    approved: {
+      bg: 'green',
+      text: 'Aprovada',
+    },
+    failure: {
+      bg: 'red',
+      text: 'Falha',
+    },
   };
 
   const TableData = ({ children }) => (
@@ -46,12 +54,25 @@ export default function Details() {
   return (
     <Template slider={<AdminMenu />} group={<></>} flexDir="column">
       <Heading>Detalhes do Pedido</Heading>
+      {data.length ? (
+        <div
+          style={{
+            backgroundColor: orderStatuses[data[0].order_status].bg,
+            width: 'max-content',
+            padding: 5,
+            borderRadius: 6,
+            marginTop: 10,
+          }}
+        >
+          <Text>{orderStatuses[data[0].order_status].text}</Text>
+        </div>
+      ) : null}
       <br />
       <Flex flexDir="row" width="100%">
-        {data ? (
+        {data.length ? (
           <table style={{ width: '100%' }}>
-            <th>Status Ordem</th>
             <th>Método de pagamento</th>
+            <th>Produto</th>
             <th>Preço</th>
             <th>Quantidade</th>
             <th>Comprador</th>
@@ -65,11 +86,10 @@ export default function Details() {
                 }}
               >
                 <TableData>
-                  <Text>{orderStatuses[item.order_status] || '-'}</Text>
-                </TableData>
-
-                <TableData>
                   <Text>{item.payment_method}</Text>
+                </TableData>
+                <TableData>
+                  <Text>{item.product_name}</Text>
                 </TableData>
                 <TableData>
                   <Text>R$ {item.price}</Text>
