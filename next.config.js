238aaf03ab9fe206731fce85c46a0c68ef0f6daa
Lucas Becker <lucasgabrielbec@gmail.com>
2022-10-28 21:58:38 -0300

    feat: adiciona variavel correta por ambiente

--- next.config.js
+++ next.config.js
@@ -1,9 +1,32 @@
 const withImages = require('next-images');
 
-module.exports = withImages({
-  esModule: true,
-  distDir: 'out',
-  env: {
-    BACKEND_URL: process.env.BACKEND_URL,
-  },
-});
+
+module.exports = (phase) => {
+  const isDev = phase === PHASE_DEVELOPMENT_SERVER
+
+  // when `next build` or `npm run build` is used
+  const isProd = phase === PHASE_PRODUCTION_BUILD && process.env.STAGING !== '1'
+
+  // when `next build` or `npm run build` is used
+  const isStaging =
+    phase === PHASE_PRODUCTION_BUILD && process.env.STAGING === '1'
+
+  const flyIoUrl = "https://liconnection-adonis.fly.dev"
+
+  const env = {
+    BACKEND_URL: (() => {
+      if (isDev) return 'http://localhost:3333'
+      if (isProd || isStaging) return flyIoUrl
+    })(),
+
+  }
+
+
+  return {
+    default: withImages({
+      esModule: true,
+      distDir: 'out',
+    }),
+    env,
+  }
+}
--- src/pages/admin/products/excel.tsx
+++ src/pages/admin/products/excel.tsx
@@ -13,17 +13,8 @@ import AdminMenu from '../../../components/Admin/Menu';
 import Bread from '../../../components/Breadcrumb';
 import { updateToast } from '../../../config/toastMessages';
 import { useToast } from '../../../hooks/toast';
-// import { post } from '../../../services/API';
-// import { api } from '../../../services/API/index';
-import { validationErrors } from '../../../services/validateForm';
 
-import {
-  checkExtension,
-  checkFormat,
-  formatSend,
-  // eslint-disable-next-line prettier/prettier
-  sheetToJson,
-} from '../../../utils/uploadExcel';
+import { checkExtension, sheetToJson } from '../../../utils/uploadExcel';
 
 const moduleName = 'products/excel';
 export default function Excel() {
@@ -74,38 +65,13 @@ export default function Excel() {
     if (!validExtension) {
       const msg = 'Extenção inválida! deve ser: csv, ods, xlsx, xls';
       addToast({
-        type: 'error',
-        title: 'ERRO!',
+        ...updateToast.error,
         description: msg,
       });
       return;
     }
     const parsedData = await sheetToJson(file);
-    // const { valid, error } = await checkFormat(parsedData);
-    // if (!valid) {
-    //   if (error.type === 'required') {
-    //     inputFileRef.current.value = null;
-    //     error.errors.forEach((errorMessage: string) => {
-    //       addToast({
-    //         type: 'error',
-    //         title: `A coluna ${errorMessage.split(' ')[0]} é obrigatória`,
-    //         description:
-    //           'O valor pode ser vazio mas a coluna deve existir na planilha',
-    //       });
-    //     });
-    //   } else {
-    //     addToast({
-    //       type: 'error',
-    //       title:
-    //         'Todos os campos requiridos existem, mas houve algum outro problema',
-    //     });
-    //   }
-
-    //   return;
-    // }
     setCanImport(true);
-    console.log('parsedData: ', parsedData);
-    // const excelData: any = formatSend(parsedData);
     setExcel(parsedData as any);
   };
 
